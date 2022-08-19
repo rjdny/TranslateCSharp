bd022e44b48be4e1995d7b8c161df14b0e7a4b5d
Ryan Downey <105568842+rjdny@users.noreply.github.com>
2022-08-19 14:04:04 -0500

    Initial Commit
    added some files

---
+++ PlaceNames/Translation.js
@@ -0,0 +1,18 @@
+﻿const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+];
+
+console.log("All Pace Names");
+
+for (const name of names) {
+    console.log(name);
+}
+
+console.log("");
+
+const theNames = names.filter((name) => name.startsWith("The"));
+
+for (const name of theNames) {
+    console.log(name);
+}
